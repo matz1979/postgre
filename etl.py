720e878184241da9f5975982f3a235785dd323c4
Matthias Engels <matthias.engels@gmail.com>
2020-01-23 22:03:10 +0000

    corr little errors

--- etl.py
+++ etl.py
@@ -14,7 +14,7 @@ def process_song_file(cur, filepath):
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = list(df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values[0)
+    artist_data = list(df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values[0])
     cur.execute(artist_table_insert, artist_data)
 
 
