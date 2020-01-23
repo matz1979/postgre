32f4c8099a523bbeda6f8e7a5ca6982242a48bc0
Matthias Engels <matthias.engels@gmail.com>
2020-01-23 21:41:01 +0000

    final version 1/23/2020

--- etl.py
+++ etl.py
@@ -7,37 +7,37 @@ from sql_queries import *
 
 def process_song_file(cur, filepath):
     # open song file
-    df = 
+    df = 'data\song_data'
 
     # insert song record
-    song_data = 
+    song_data =  list(df[['song_id', 'title', 'artist_id', 'year', 'duration']].values[0])
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = 
+    artist_data = list(df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values[0)
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df[df['page'] == 'NextSong']
 
     # convert timestamp column to datetime
-    t = 
+    t = pd.to_datetime(df['ts'],unit='ms')
     
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = [df['ts'].values.tolist(),t.dt.hour.values.tolist(),t.dt.day.values.tolist(),t.dt.week.values.tolist(),t.dt.month.values.tolist(),t.dt.year.values.tolist(),t.dt.weekday_name.values.tolist()]
+    column_labels = ('start_time','hour','day','week_of_year','month','year','weekday')
+    time_df = pd.DataFrame(dict(zip(column_labels, time_data)))
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level']]
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -51,7 +51,7 @@ def process_log_file(cur, filepath):
         songid, artistid = results if results else None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = (index, row.ts, row.userId, row.level, songid, artistid, row.sessonId, row.artist_location, row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
 
--- sql_queries.py
+++ sql_queries.py
@@ -1,55 +1,62 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE songplays;"
+user_table_drop = "DROP TABLE users;"
+song_table_drop = "DROP TABLE songs;"
+artist_table_drop = "DROP TABLE artist;"
+time_table_drop = "DROP TABLE time;"
 
 # CREATE TABLES
 
 songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays
-                    (songplay_id int, start_time int, user_id, level int, song_id int, artist_id int, session_id int, location varchar, user_agent varchar)
+                    (songplay_id int NOT NULL, start_time timestamp, user_id, level varchar, song_id int, artist_id int, session_id int, location varchar, user_agent varchar)
 """)
 
-user_table_create = ("""CREATE TABLE IF NOT EXISTS users (user_id int, first_name varchar, last_name varchar, gender varchar, level int)
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users 
+                    (user_id int NOT NULL, first_name varchar, last_name varchar, gender varchar, level varchar)
 """)
 
-song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (song_id int, title varchar, artist_id int, year int, duration numeric)
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs 
+                    (song_id int NOT NULL, title varchar, artist_id int, year int, duration timestamp)
 """)
 
-artist_table_create = ("""CREATE TABLE IF NOT EXISTS artist(artist_id int, name varchar, location varchar, latitude numeric, longitude numeric)
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artist
+                    (artist_id int NOT NULL, name varchar, location varchar, latitude numeric, longitude numeric)
 """)
 
-time_table_create = ("""CREATE TABLE IF NOT EXISTS time(start_time numeric, hour numeric, day numeric, week numeric, month numeric, year int, weekday varchar)
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time 
+                    (id int NOT NULL start_time timestamp, hour numeric, day numeric, week numeric, month numeric, year int, weekday varchar)
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""INSERT INTO songplays (songplay_id, start_time, user_id, level, artist_id, sesson_id, location, user_agent)
-                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s), ();
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s), (songplay_data);
 """)
 
 user_table_insert = ("""INSERT INTO user (user_id, first_name, last_name, gender, level)
-                        VALUES (%s, %s, %s, %s, %s), ();
+                        VALUES (%s, %s, %s, %s, %s), (user_df);
 """)
 
 song_table_insert = ("""INSERT INTO song (song_id, title, artist_id, year, duration)
-                        VALUES (%s, %s, %s, %s ,%s), ();
+                        VALUES (%s, %s, %s, %s ,%s), (song_data);
 """)
 
 artist_table_insert = ("""INSERT INTO artist (artist_id, name, location, latitude, longitude)
-                        VALUES (%s, %s, %s, %s, %s), ();
+                        VALUES (%s, %s, %s, %s, %s), (artist_data);
 """)
 
 
-time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, year, weekday)
-                        VALUES (%s, %s, %s, %s, %s, %s ,%s), ();
+time_table_insert = ("""INSERT INTO time (id, start_time, hour, day, week, month, year, weekday)
+                        VALUES (%s, %s, %s, %s, %s, %s, %s ,%s), (time_df);
 """)
 
 # FIND SONGS
 
-song_select = ("""
+song_select = ("""SELECT s.song_id, a.artist_id
+                FROM songs s
+                JOIN artist a ON (s.artist_id = a.artist_id)
+                WHERE (s.title = %s AND a.name = %s AND s.duration = %s);
 """)
 
 # QUERY LISTS
