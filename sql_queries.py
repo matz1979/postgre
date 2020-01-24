e1bf1bd921f9acb7eb54dc913af1bd5a42245544
Matthias Engels <matthias.engels@gmail.com>
2020-01-24 19:10:29 +0000

    fix little errors

--- sql_queries.py
+++ sql_queries.py
@@ -1,69 +1,80 @@
 # DROP TABLES
 
-songplay_table_drop = "DROP TABLE songplays;"
-user_table_drop = "DROP TABLE users;"
-song_table_drop = "DROP TABLE songs;"
-artist_table_drop = "DROP TABLE artist;"
-time_table_drop = "DROP TABLE time;"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artist;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
-songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays
-                    (songplay_id int NOT NULL, 
+songplay_table_create = ("""CREATE TABLE songplays
+                    (
+                    songplay_id int, 
                     start_time timestamp without time zone, 
-                    user_id int NOT NULL,
+                    user_id int,
                     level varchar,
-                    song_id int NOT NULL,
-                    artist_id int NOT NULL,
-                    session_id int NOT NULL,
+                    song_id int,
+                    artist_id int,
+                    session_id int,
                     location varchar,
                     user_agent varchar,
-                    PRIMARY KEY (user_id, song_id, artist_id),
+                    PRIMARY KEY (user_id, song_id, artist_id, start_time),
                     CONSTRAINT songplays_user_user_id_fkey FOREIGN KEY (user_id)
-                        REFERENCE users (user_id),
+                        REFERENCES users (user_id) MATCH SIMPLE
+                         ON UPDATE NO ACTION ON DELETE NO ACTION,
                     CONSTRAINT songplays_songs_songs_id_fkey FOREIGN KEY (song_id)
-                        REFERENCE songs (song_id),
+                        REFERENCES songs (song_id) MATCH SIMPLE
+                          ON UPDATE NO ACTION ON DELETE NO ACTION,
                     CONSTRAINT songplays_artist_artist_id_fkey FOREIGN KEY (artist_id)
-                        REFERENCE artist (artist_id),
+                        REFERENCES artist (artist_id) MATCH SIMPLE
+                          ON UPDATE NO ACTION ON DELETE NO ACTION,
+                    CONSTRAINT songplays_time_start_time_fkey FOREIGN KEY (start_time)
+                        REFERENCES time (start_time) MATCH SIMPLE
+                          ON UPDATE NO ACTION ON DELETE NO ACTION
                     );
 """)
 
-user_table_create = ("""CREATE TABLE IF NOT EXISTS users 
-                    (user_id int NOT NULL PRIMARY KEY,
+user_table_create = ("""CREATE TABLE users 
+                    (
+                    user_id int PRIMARY KEY,
                     first_name varchar,
                     last_name varchar,
                     gender varchar,
-                    level varchar,
+                    level varchar
                     );
 """)
 
-song_table_create = ("""CREATE TABLE IF NOT EXISTS songs 
-                    (song_id int NOT NULL PRIMARY KEY,
+song_table_create = ("""CREATE TABLE songs 
+                    (
+                    song_id int PRIMARY KEY,
                     title varchar,
                     artist_id int,
                     year int,
-                    duration timestamp without timezone,
+                    duration timestamp without timezone
                     );
 """)
 
-artist_table_create = ("""CREATE TABLE IF NOT EXISTS artist
-                    (artist_id int NOT NULL PRIMARY KEY,
+artist_table_create = ("""CREATE TABLE artist
+                    (
+                    artist_id int PRIMARY KEY,
                     name varchar,
                     location varchar,
                     latitude numeric,
-                    longitude numeric,
+                    longitude numeric
                     );
 """)
 
-time_table_create = ("""CREATE TABLE IF NOT EXISTS time 
-                    (id serial PRIMARY KEY,
-                    start_time timestamp without timezone,
+time_table_create = ("""CREATE TABLE time 
+                    (
+                    start_time timestamp without timezone PRIMARY KEY,
                     hour numeric,
                     day numeric,
                     week numeric,
                     month numeric,
                     year int,
-                    weekday varchar);
+                    weekday varchar
+                    );
 """)
 
 # INSERT RECORDS
