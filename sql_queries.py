6d75795517a0a6ea1d86dac8a93ffaa76833e40f
Matthias Engels <matthias.engels@gmail.com>
2020-01-24 17:49:29 +0000

    add star schema

--- sql_queries.py
+++ sql_queries.py
@@ -9,23 +9,61 @@ time_table_drop = "DROP TABLE time;"
 # CREATE TABLES
 
 songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays
-                    (songplay_id int NOT NULL, start_time timestamp, user_id, level varchar, song_id int, artist_id int, session_id int, location varchar, user_agent varchar);
+                    (songplay_id int NOT NULL, 
+                    start_time timestamp without time zone, 
+                    user_id int NOT NULL,
+                    level varchar,
+                    song_id int NOT NULL,
+                    artist_id int NOT NULL,
+                    session_id int NOT NULL,
+                    location varchar,
+                    user_agent varchar,
+                    PRIMARY KEY (user_id, song_id, artist_id),
+                    CONSTRAINT songplays_user_user_id_fkey FOREIGN KEY (user_id)
+                        REFERENCE users (user_id),
+                    CONSTRAINT songplays_songs_songs_id_fkey FOREIGN KEY (song_id)
+                        REFERENCE songs (song_id),
+                    CONSTRAINT songplays_artist_artist_id_fkey FOREIGN KEY (artist_id)
+                        REFERENCE artist (artist_id),
+                    );
 """)
 
 user_table_create = ("""CREATE TABLE IF NOT EXISTS users 
-                    (user_id int NOT NULL, first_name varchar, last_name varchar, gender varchar, level varchar);
+                    (user_id int NOT NULL PRIMARY KEY,
+                    first_name varchar,
+                    last_name varchar,
+                    gender varchar,
+                    level varchar,
+                    );
 """)
 
 song_table_create = ("""CREATE TABLE IF NOT EXISTS songs 
-                    (song_id int NOT NULL, title varchar, artist_id int, year int, duration timestamp);
+                    (song_id int NOT NULL PRIMARY KEY,
+                    title varchar,
+                    artist_id int,
+                    year int,
+                    duration timestamp without timezone,
+                    );
 """)
 
 artist_table_create = ("""CREATE TABLE IF NOT EXISTS artist
-                    (artist_id int NOT NULL, name varchar, location varchar, latitude numeric, longitude numeric);
+                    (artist_id int NOT NULL PRIMARY KEY,
+                    name varchar,
+                    location varchar,
+                    latitude numeric,
+                    longitude numeric,
+                    );
 """)
 
 time_table_create = ("""CREATE TABLE IF NOT EXISTS time 
-                    (id int NOT NULL start_time timestamp, hour numeric, day numeric, week numeric, month numeric, year int, weekday varchar);
+                    (id serial PRIMARY KEY,
+                    start_time timestamp without timezone,
+                    hour numeric,
+                    day numeric,
+                    week numeric,
+                    month numeric,
+                    year int,
+                    weekday varchar);
 """)
 
 # INSERT RECORDS
